bd77fd3dcb35a7ded603e20e81ad1b8bc4370136
Ayush <113465008+ayush1944@users.noreply.github.com>
2025-09-30 23:16:30 +0530

    alladm

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api/auth",
+  baseURL: import.meta.env.VITE_BACKEND_URL
+    ? `${import.meta.env.VITE_BACKEND_URL}/api/auth`
+    : "http://localhost:3000/api/auth",
   withCredentials: true,
 });
 
