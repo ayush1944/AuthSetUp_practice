6809e9a0580801a9bba27d431d42cff71474550e
Ayush <113465008+ayush1944@users.noreply.github.com>
2025-09-30 23:14:02 +0530

    asdsafds

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,17 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // make sure installed
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/", // good practice
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: `${import.meta.env.VITE_BACKEND_URL}` || "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+  plugins: [
+    tailwindcss(),
+  ],
+})
